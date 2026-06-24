4f6deea889158c4f8573015549faa1282c2a6369
sofia <lanziqi955@163.com>
2026-06-24 20:15:56 +0800

    修改路径

--- pages/index.vue
+++ pages/index.vue
@@ -63,15 +63,18 @@
 </template>
 
 <script setup lang="ts">
-import position1 from 'public/positions/1.json'
-import position2 from 'public/positions/2.json'
-import position3 from 'public/positions/3.json'
-import position4 from 'public/positions/4.json'
-import position5 from 'public/positions/5.json'
-import position6 from 'public/positions/6.json'
-import position7 from 'public/positions/7.json'
-import position8 from 'public/positions/8.json'
-import position9 from 'public/positions/9.json'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import position1 from '../public/positions/1.json'
+import position2 from '../public/positions/2.json'
+import position3 from '../public/positions/3.json'
+import position4 from '../public/positions/4.json'
+import position5 from '../public/positions/5.json'
+import position6 from '../public/positions/6.json'
+import position7 from '../public/positions/7.json'
+import position8 from '../public/positions/8.json'
+import position9 from '../public/positions/9.json'
 
 const value = ref('')
 const value2 = ref('1')
