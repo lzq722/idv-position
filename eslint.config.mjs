49613638c5647996dbbeab7eb0b9d34b84a5f3e1
lzq <lanziqi955@163.com>
2026-06-25 15:49:15 +0800

    修改配置文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,17 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-// https://eslint.nuxt.com/packages/module
 export default withNuxt(
   {
     rules: {
       'vue/multi-word-component-names': 'off',
-    }
-  }
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
+    },
+  },
+  {
+    ignores: [
+      'docs/**',
+      '.output/**',
+      '.nuxt/**',
+      'dist/**',
+    ],
+  },
 )
